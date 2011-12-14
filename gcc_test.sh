258313402b767564ffe05e44f28aeae0787bfabc
beeender <chenmulong@gmail.com>
2011-12-14 16:00:23 +0800

    Rename test.sh to gcc_test.sh.
    And revert the changes back.

--- gcc_test.sh
+++ gcc_test.sh
@@ -345,91 +345,91 @@ base_tests() {
         fi
     fi
 
-    #testname="compilercheck=mtime"
-    #$CCACHE -Cz >/dev/null
-    #cat >compiler.sh <<EOF
-##!/bin/sh
-#CCACHE_DISABLE=1 # If $COMPILER happens to be a ccache symlink...
-#export CCACHE_DISABLE
-#exec $COMPILER "\$@"
-## A comment
-#EOF
-    #chmod +x compiler.sh
-    #backdate compiler.sh
-    #CCACHE_COMPILERCHECK=mtime $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 0
-    #checkstat 'cache miss' 1
-    #sed_in_place 's/comment/yoghurt/' compiler.sh # Don't change the size
-    #chmod +x compiler.sh
-    #backdate compiler.sh
-    #CCACHE_COMPILERCHECK=mtime $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 1
-    #checkstat 'cache miss' 1
-    #touch compiler.sh
-    #CCACHE_COMPILERCHECK=mtime $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 1
-    #checkstat 'cache miss' 2
+    testname="compilercheck=mtime"
+    $CCACHE -Cz >/dev/null
+    cat >compiler.sh <<EOF
+#!/bin/sh
+CCACHE_DISABLE=1 # If $COMPILER happens to be a ccache symlink...
+export CCACHE_DISABLE
+exec $COMPILER "\$@"
+# A comment
+EOF
+    chmod +x compiler.sh
+    backdate compiler.sh
+    CCACHE_COMPILERCHECK=mtime $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    sed_in_place 's/comment/yoghurt/' compiler.sh # Don't change the size
+    chmod +x compiler.sh
+    backdate compiler.sh
+    CCACHE_COMPILERCHECK=mtime $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
+    touch compiler.sh
+    CCACHE_COMPILERCHECK=mtime $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 2
 
-    #testname="compilercheck=content"
-    #$CCACHE -z >/dev/null
-    #CCACHE_COMPILERCHECK=content $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 0
-    #checkstat 'cache miss' 1
-    #backdate compiler.sh
-    #CCACHE_COMPILERCHECK=content $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 1
-    #checkstat 'cache miss' 1
-    #echo "# Compiler upgrade" >>compiler.sh
-    #backdate compiler.sh
-    #CCACHE_COMPILERCHECK=content $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 1
-    #checkstat 'cache miss' 2
+    testname="compilercheck=content"
+    $CCACHE -z >/dev/null
+    CCACHE_COMPILERCHECK=content $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    backdate compiler.sh
+    CCACHE_COMPILERCHECK=content $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
+    echo "# Compiler upgrade" >>compiler.sh
+    backdate compiler.sh
+    CCACHE_COMPILERCHECK=content $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 2
 
-    #testname="compilercheck=none"
-    #$CCACHE -z >/dev/null
-    #backdate compiler.sh
-    #CCACHE_COMPILERCHECK=none $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 0
-    #checkstat 'cache miss' 1
-    #CCACHE_COMPILERCHECK=none $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 1
-    #checkstat 'cache miss' 1
-    #echo "# Compiler upgrade" >>compiler.sh
-    #CCACHE_COMPILERCHECK=none $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 2
-    #checkstat 'cache miss' 1
+    testname="compilercheck=none"
+    $CCACHE -z >/dev/null
+    backdate compiler.sh
+    CCACHE_COMPILERCHECK=none $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    CCACHE_COMPILERCHECK=none $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
+    echo "# Compiler upgrade" >>compiler.sh
+    CCACHE_COMPILERCHECK=none $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 2
+    checkstat 'cache miss' 1
 
-    #testname="compilercheck=command"
-    #$CCACHE -z >/dev/null
-    #backdate compiler.sh
-    #CCACHE_COMPILERCHECK='echo %compiler%' $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 0
-    #checkstat 'cache miss' 1
-    #echo "# Compiler upgrade" >>compiler.sh
-    #CCACHE_COMPILERCHECK="echo ./compiler.sh" $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 1
-    #checkstat 'cache miss' 1
-    #cat <<EOF >foobar.sh
-##!/bin/sh
-#echo foo
-#echo bar
-#EOF
-    #chmod +x foobar.sh
-    #CCACHE_COMPILERCHECK='./foobar.sh' $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 1
-    #checkstat 'cache miss' 2
-    #CCACHE_COMPILERCHECK='echo foo; echo bar' $CCACHE ./compiler.sh -c test1.c
-    #checkstat 'cache hit (preprocessed)' 2
-    #checkstat 'cache miss' 2
+    testname="compilercheck=command"
+    $CCACHE -z >/dev/null
+    backdate compiler.sh
+    CCACHE_COMPILERCHECK='echo %compiler%' $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 0
+    checkstat 'cache miss' 1
+    echo "# Compiler upgrade" >>compiler.sh
+    CCACHE_COMPILERCHECK="echo ./compiler.sh" $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 1
+    cat <<EOF >foobar.sh
+#!/bin/sh
+echo foo
+echo bar
+EOF
+    chmod +x foobar.sh
+    CCACHE_COMPILERCHECK='./foobar.sh' $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 1
+    checkstat 'cache miss' 2
+    CCACHE_COMPILERCHECK='echo foo; echo bar' $CCACHE ./compiler.sh -c test1.c
+    checkstat 'cache hit (preprocessed)' 2
+    checkstat 'cache miss' 2
 
-    #testname="compilercheck=unknown_command"
-    #$CCACHE -z >/dev/null
-    #backdate compiler.sh
-    #CCACHE_COMPILERCHECK="unknown_command" $CCACHE ./compiler.sh -c test1.c 2>/dev/null
-    #if [ "$?" -eq 0 ]; then
-        #test_failed "Expected failure running unknown_command to verify compiler but was success"
-    #fi
-    #checkstat 'compiler check failed' 1
+    testname="compilercheck=unknown_command"
+    $CCACHE -z >/dev/null
+    backdate compiler.sh
+    CCACHE_COMPILERCHECK="unknown_command" $CCACHE ./compiler.sh -c test1.c 2>/dev/null
+    if [ "$?" -eq 0 ]; then
+        test_failed "Expected failure running unknown_command to verify compiler but was success"
+    fi
+    checkstat 'compiler check failed' 1
 
     testname="no object file"
     cat <<'EOF' >test_no_obj.c
